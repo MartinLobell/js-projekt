7787ccdc78c3b87658810f38a55b323101ec1ad1
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-05-30 00:19:06 +0200

    Lade till mapp för componenter som motsvarar olika sidor på hemsidan

---
+++ react-app/src/components/pages/Battle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MyPokemons extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Battle page</h1>
+                <p>Bla bla ...</p>
+            </div>
+        )
+    }
+}
---
+++ react-app/src/components/pages/MyPokemons.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MyPokemons extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My pokemon cards</h1>
+                <p>Bla bla ...</p>
+            </div>
+        )
+    }
+}
