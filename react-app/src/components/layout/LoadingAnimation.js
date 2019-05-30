d52ca4b25f0421064f2aa96a2f71be0cc5ab8db0
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-05-30 20:21:03 +0200

    Komponent med animering

---
+++ react-app/src/components/layout/LoadingAnimation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+export default function Menu() {
+  const imageBoxStyle = {
+    width: "100%",
+    margin: "auto",
+    maxWidth: "500px",
+  }
+
+  const imageStyle = {
+    width: "100%",
+  }
+  
+  return (
+    <div style={imageBoxStyle}>
+        <h2>Loading pokemon...</h2>
+        <img
+            src="https://i.pinimg.com/originals/4e/a2/3e/4ea23e6339937b95a8aa5cd08eeb3266.gif"
+            alt="pokemon loading gif"
+            style={imageStyle}
+        />
+    </div>
+  )
+  
+}
