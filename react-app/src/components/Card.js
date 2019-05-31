e0ccb7266ab68dcb2137a99f444e932160446680
Tobias <tobiashakanthoren@gmail.com>
2019-05-31 15:29:24 +0200

    onclick for selecting cards

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Card = ({ name, image }) => {
+function setPokemon(id, name ){
+	localStorage.setItem(id, name);
+	console.log(localStorage.getItem(id));
+}
+
+const Card = ({ name, image, id }) => {
     return (
-        <div className="tc bg-light-green dib dim br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-green dib dim br3 pa3 ma2 grow bw2 shadow-5" onClick={ () => {setPokemon(id, name) }} >
             <img src={ image } alt="Pokemon"/>
             <div>
                 <h2>{ name }</h2>
