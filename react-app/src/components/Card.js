61cff5052329bc16d7e352d6b216ba4e0cccebab
Tobias <tobiashakanthoren@gmail.com>
2019-06-02 06:58:40 +0200

    Removed quick prototype function

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -1,20 +1,5 @@
 import React from 'react';
 
-function setPokemon(name){
-    const localId = localStorage.length;
-    if (localId === 0) {
-        var myPokemon = {};
-        myPokemon[name] = name;
-        localStorage.setItem('myPokemon', JSON.stringify(myPokemon));
-    } else {
-        var myPokemon = JSON.parse(localStorage.getItem('myPokemon'));
-        myPokemon[name] = name;
-    }
-    localStorage.setItem('myPokemon', JSON.stringify(myPokemon));
-
-    console.log(JSON.parse(localStorage.getItem('myPokemon')));
-}
-
 const Card = ({ id, name, image, saveCard }) => {
     return (
         <div className="tc bg-light-green dib dim br3 pa3 ma2 grow bw2 shadow-5" onClick={saveCard} data-id={id} data-name={name}>
