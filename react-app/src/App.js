bb74f4b33b6907281124da3d3fc152068d0e8e81
Martin Lobell <martin.lobell@gmail.com>
2019-06-03 10:57:59 +0200

    The URL journey
    Tried to get fewer doublets.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -26,10 +26,9 @@ class App extends Component {
         this.loadCardsToState(); // Hämtar sparade kort från LS
     }
 
-
     // Hämtar pokes från API
     getPokeCard = () => {
-        fetch(`https://api.pokemontcg.io/v1/cards/?setCode=base1|base2|base3&supertype=Pokemon&pageSize=500`)
+        fetch(`https://api.pokemontcg.io/v1/cards/?setCode=base1|base2|base3&rarity=Common|Uncommon|Rare&supertype=Pokemon&pageSize=500`)
             .then(res => res.json())
             .then(json => {
                 this.setState({
